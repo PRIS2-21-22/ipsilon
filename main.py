674c932af9aa86fc85df229fcf3c065c07223857
Juan Pablo Grosso Tarazaga <jgt627@inlumine.ual.es>
2021-03-17 20:10:20 +0100

    clases añadidas

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("saludos")
+
+if __name__ == "__main__":
+    main()
--- polinomio.py
+++ polinomio.py
@@ -1,4 +1,36 @@
 class Polinomio():
-    a = 4
-    b = 0
-    c = a - b
+
+        def __init__(self, value):
+            self.polinomio = value
+    
+
+    def suma(otroPolinomio):
+        if len(self.polinomio > otroPolinomio):
+            solucion = self.polinomio
+
+            for x in range(len(otroPolinomio)):
+                solucion[x] += otroPolinomio[x]
+        else:
+            solucion = otroPolinomio
+
+            for x in range(len(self.polinomio)):
+                solucion[x] += self. polinomio[x]   
+        
+        return solucion
+
+    def resta(otroPolinomio):
+        if len(self.polinomio > otroPolinomio):
+            solucion = self.polinomio
+
+            for x in range(len(otroPolinomio)):
+                solucion[x] -= otroPolinomio[x]
+        else:
+            solucion = otroPolinomio
+
+            for x in range(len(self.polinomio)):
+                solucion[x] -= self. polinomio[x]   
+        
+        return solucion
+
+    def producto(otroPolinomio):
+        solucion = [0] * (len(self.polinomio) + len(otroPolinomio))
